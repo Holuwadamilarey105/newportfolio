8bf900f4b3255dfadd53e82e5f542ed92227cac2
Darey <ayubaibrahim105@gmail.com>
2025-07-29 17:07:07 -0700

    Add new Render domain to ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,11 +26,7 @@ SECRET_KEY = 'django-insecure-*45!nr)z%8#he04^f*i@5c9e142k*&un4qrfb2zdi-z^^78)e%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'newportfolio.onrender.com',
-    'newportfolio-6-fdgh.onrender.com',
-    'newportfolio-6-pb5k.onrender.com'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
